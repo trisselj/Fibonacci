42fbdf679c0e9b0aea414208ddcef33125b9dc44
trisselj <trisselj@oregonstate.edu>
2024-07-17 17:45:14 -0700

    q

--- fib.py
+++ fib.py
@@ -8,8 +8,9 @@ def fib(n):
     a, b =1, 1 #Definig the first two numbers in the sequence
     for _ in range(2, n): #Loop for calculating Fibonacci sequence up to nth integer
         a, b = b, a + b
-        if n > 1:
-            return b
-        else:
-            return a
+    if n > 1:
+        return b
+    else:
+        return a
     
+print(fib(10))
